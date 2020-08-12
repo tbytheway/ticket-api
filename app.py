da201e6077d86106dcd4edc759bdc102581bf29d
tbytheway <tdbytheway@gmail.com>
2020-08-11 19:45:07 -0600

    gunicorn environs psycopg2

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from flask_heroku import Heroku
+from environs import Env
 
 import os
 
